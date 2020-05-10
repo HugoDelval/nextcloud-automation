8d924616f3425d01e6ed5524863b14b3abc948c7
Hugo Delval <hugo.delval@gmail.com>
2020-05-10 12:58:04 +0200

    fix for non-interactives backups

--- ansible/roles/nextcloud/templates/backup.sh
+++ ansible/roles/nextcloud/templates/backup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec --user www-data app php occ maintenance:mode --on
+/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec -T --user www-data app php occ maintenance:mode --on
 
 cleanup() {
     rv=$?
-    /usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec --user www-data app php occ maintenance:mode --off
-    rm -rf ${BACKUP_FOLDER}
+    /usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec -T --user www-data app php occ maintenance:mode --off
     exit $rv
 }
 trap "cleanup" INT TERM EXIT
@@ -18,9 +17,10 @@ BACKUP_FOLDER=/var/backups/nextcloud-backup_${DATE}
 
 cd /var/disk1/
 rsync -Aavx nextcloud_var_www_html/ ${BACKUP_FOLDER}
-tar cfz ${BACKUP_FOLDER}-html.tgz ${BACKUP_FOLDER}
+tar cfz ${BACKUP_FOLDER}-html.tgz -C ${BACKUP_FOLDER} .
+rm -rf ${BACKUP_FOLDER}
 
-/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec --user postgres db pg_dump -U {{ POSTGRES_USER }} {{ POSTGRES_DB }} | gzip -c > ${BACKUP_FOLDER}-postgres.gz
+/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec -T --user postgres db pg_dump -U {{ POSTGRES_USER }} {{ POSTGRES_DB }} | gzip -c > ${BACKUP_FOLDER}-postgres.gz
 
 files_to_remove=$(ls -t /var/backups/nextcloud-backup_* | tail -n +{{ number_of_backups_to_keep_on_disk*2 }})
 
@@ -40,15 +40,16 @@ export OS_REGION_NAME={{ OS_REGION_NAME }}
 export OS_INTERFACE={{ OS_INTERFACE }}
 export OS_IDENTITY_API_VERSION={{ OS_IDENTITY_API_VERSION }}
 
-swift upload {{ swift_bucket_name }} ${BACKUP_FOLDER}-postgres.gz
-swift upload {{ swift_bucket_name }} -S 1073741824 ${BACKUP_FOLDER}.tgz
+/usr/local/bin/swift upload --object-name "nextcloud-backup_${DATE}-postgres.gz" {{ swift_bucket_name }} ${BACKUP_FOLDER}-postgres.gz
+/usr/local/bin/swift upload --object-name "nextcloud-backup_${DATE}-html.tgz" {{ swift_bucket_name }} -S 1073741824 ${BACKUP_FOLDER}-html.tgz
 
 # cleanup old files
-for file in $(swift list -l {{ swift_bucket_name }}); do
+for file in $(/usr/local/bin/swift list {{ swift_bucket_name }}); do
     file_date=$(echo $file | grep -oP "nextcloud\-backup_\K\d{8}")
+    [[ -z "$file_date" ]] && continue
     DATE_DIFF=$((($(date +%s)-$(date --date="${file_date}" +%s))/(60*60*24)))
     if [ "${DATE_DIFF}" -gt "{{ number_of_backups_to_keep_on_swift }}" ]; then
-        swift delete {{ swift_bucket_name }} $file
+        /usr/local/bin/swift delete {{ swift_bucket_name }} $file
     fi
 done
 
--- ansible/roles/nextcloud/templates/restore_backup.sh
+++ ansible/roles/nextcloud/templates/restore_backup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -euxo pipefail
-
 if (( $# != 1 )); then
     echo "Usage:"
     echo "    restore-backup-nextcloud.sh name_of_backup"
     echo
     echo "    eg: if you want to restore the backup of the 31st of December, 2020:"
-    echo "        restore_backup.sh nextcloud-backup_20201231"
+    echo "        restore-backup-nextcloud.sh nextcloud-backup_20201231"
     exit 1
 fi
+
+set -euxo pipefail
 
 BACKUP_TO_RESTORE="$1"
 
@@ -42,18 +42,20 @@ if [[ ! -f "$db_file" && ! -f "$files" ]]; then
     export OS_INTERFACE={{ OS_INTERFACE }}
     export OS_IDENTITY_API_VERSION={{ OS_IDENTITY_API_VERSION }}
 
-    swift download {{ swift_bucket_name }} $db_file
-    swift download {{ swift_bucket_name }} $files
+    /usr/local/bin/swift download {{ swift_bucket_name }} $db_file
+    /usr/local/bin/swift download {{ swift_bucket_name }} $files
 fi
 
 {% endif %}
 
 [[ ! -f "$db_file" || ! -f "$files" ]] && echo "File '$db_file' or '$files' is absent on disk, aborting." && exit 1
 
+mkdir nextcloud-files
 tar xvzf "$files" -C ./nextcloud-files # --strip-components=1
 
-rsync -Aax ./nextcloud-files /var/disk1/nextcloud_var_www_html/
+find /var/disk1/nextcloud_var_www_html/ -depth -type d -empty -delete
+rsync --delete-after -Aax ./nextcloud-files /var/disk1/nextcloud_var_www_html/
 
-/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec --user postgres db dropdb -U {{ POSTGRES_USER }} {{ POSTGRES_DB }}
-/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec --user postgres db createdb -U {{ POSTGRES_USER }} -O {{ POSTGRES_USER }} {{ POSTGRES_DB }}
-gunzip -c "$db_file" | /usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec --user postgres db psql -U {{ POSTGRES_USER }} {{ POSTGRES_DB }}
+/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec -T --user postgres db dropdb -U {{ POSTGRES_USER }} {{ POSTGRES_DB }}
+/usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec -T --user postgres db createdb -U {{ POSTGRES_USER }} -O {{ POSTGRES_USER }} {{ POSTGRES_DB }}
+gunzip -c "$db_file" | /usr/local/bin/docker-compose -f /srv/nextcloud/docker-compose.yml exec -T --user postgres db psql -U {{ POSTGRES_USER }} {{ POSTGRES_DB }}
